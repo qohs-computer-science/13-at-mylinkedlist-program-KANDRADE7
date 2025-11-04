6a7f2d921b31f4a793109219e6e644a78a92db58
KANDRADE7 <andradekate@icloud.com>
2025-11-04 17:48:08 +0000

    day 1 - end of class

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,9 +1,13 @@
-public class MyLLTester.java
+//My tester file 
+public class MyLLTester
 {
   public static void main(String [] args)
   {
-    
+    MyLinkedList apple = new MyLinkedList(); 
+    apple.addFirst(2);
+    System.out.println(apple.getValue());
+
 
   } // end main
+}//end 
   
-} // end class
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,57 @@
+//Katherine Andrade, 11/4/25, period 6
 public class MyLinkedList
 {
+//fields
+    private ListNode head;
+    private int size; 
+    public MyLinkedList(){
+        head = null; 
+        size = 0; 
+    }//end constructor
+
+    public int size(){
+        return size;
+    }//end size()
+
+    public boolean isEmpty(){
+        if(size == 0)
+            return true;
+        return false;
+    }// end isEmpty()
+
+    /* 
+     * public String toString(){
+        //to be added to later...
+    }//end toString()
+     * 
+     */
+    
+    public boolean addFirst(Object newItem){
+        ListNode front = (ListNode) newItem;
+        front.setNext(head);
+        head = front; 
+        size++;
+        return true; 
+    }//end addFirst()
 
 
-}
+    
+     public boolean add(Object newItem){
+        ListNode temp = head; 
+        if(temp.getValue()==null){
+            addFirst(newItem);
+            return true;
+        }
+        while(temp.getValue() != null){
+            if(temp.getNext()==null){
+                temp.setValue(newItem);
+                size++; 
+                return true; 
+            }//end if
+            else
+                temp = temp.getNext();
+        }//end while loop
+    }//end add()
+    
+    
+} //end class
